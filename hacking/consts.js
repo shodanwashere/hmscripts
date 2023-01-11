a05b04d86c8bfdf21ef8efd1bdd464771a5382e6
Nuno Dias <jookaada@protonmail.com>
2023-01-11 18:45:50 +0000

    Create consts.js

---
+++ hacking/consts.js
@@ -0,0 +1,13 @@
+function() {
+    #db.us(
+      { _id:'cn'},
+      {
+        _id:'cn',
+        u: ['unlock', 'open', 'release'],
+        p: [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97],
+        cl: "red0orange0yellow0lime0green0cyan0blue0purple".split('0'),
+        lc: "vc2c7q|6hh8xw|cmppiq|sa23uw|tvfkyq|uphlaw|xwz7ja".split('|')
+      }
+    )
+    return { ok: true }
+  }
